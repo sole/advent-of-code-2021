07f998a9a6109aac0135eaed919b57c886b54944
sole <listas@soledadpenades.com>
2021-12-03 20:11:20 +0100

    day 3: part 2 done but it is hard to write code when people talk to you

--- 03/main.js
+++ 03/main.js
@@ -43,7 +43,37 @@ let gamma = binaryArrayToNumber(mostCommonBits);
 let epsilon = binaryArrayToNumber(leastCommonBits);
 let powerConsumption = gamma * epsilon;
 
-console.log(`gamma: ${gamma}, epsilon: ${epsilon}, power consumption: ${powerConsumption}`);
+console.log(`Part 1 - gamma: ${gamma}, epsilon: ${epsilon}, power consumption: ${powerConsumption}`);
+
+let oxigenGeneratorRating = 0;
+let carbonGeneratorRating = 0;
+
+let oxigenArray = lines.concat();
+let carbonArray = lines.concat();
+
+for(let i = 0; i < maxNumberBits; i++) {
+	let commonBit = findMostCommonBitAtPosition(oxigenArray, i);
+	if(oxigenArray.length > 1) {
+		oxigenArray = oxigenArray.filter(e => {
+			let bit = Number(e[i]);
+			return bit == commonBit;
+		});
+	}
+
+	let uncommonBit = findMostCommonBitAtPosition(carbonArray, i) == 1 ? 0 : 1;
+	if(carbonArray.length > 1) {
+		carbonArray = carbonArray.filter(e => {
+			let bit = Number(e[i]);
+			return bit == uncommonBit;
+		});
+	}
+}
+
+oxigenGeneratorRating = Number.parseInt(oxigenArray.pop(), 2);
+carbonGeneratorRating = Number.parseInt(carbonArray.pop(), 2);
+let lifeSupportRating = oxigenGeneratorRating * carbonGeneratorRating;
+
+console.log(`Part 2 - generator ratings - oxigen: ${oxigenGeneratorRating}, carbon: ${carbonGeneratorRating}, life support: ${lifeSupportRating}`);
 
 function findMaxLength(arr) {
 	let lengths = arr.map(el => el.length);
@@ -55,4 +85,16 @@ function binaryArrayToNumber(arr) {
 	let binaryString = arr.join('');
 	let parsedNumber = Number.parseInt(binaryString, 2);
 	return parsedNumber;
+}
+
+function findMostCommonBitAtPosition(array, position) {
+	let countOnes = 0;
+	array.forEach(v => {
+		let bit = Number(v[position]);
+		countOnes += bit;
+	});
+	
+	// If it's most common, it's happening half or more of the times
+	let mostCommon = countOnes >= array.length / 2 ? 1 : 0;
+	return mostCommon;
 }
